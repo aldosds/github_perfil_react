466504d58d4123bff2d77ed6267dc219ca06ea0e
Aldo Santos <aldoacesso@gmail.com>
2023-11-21 16:36:08 -0300

    Utilizando a função useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 import Perfil from "./components/Perfil";
 import Formulario from "./components/Formulario";
 
 function App() {
+  const [formularioEstaVisivel, setFromularioEstaVisivel] = useState(true);
 
   return (
     <>  
       <Perfil nome="Aldo" endereco="https://github.com/aldosds.png" />
-      <Formulario/>
+      
+      {formularioEstaVisivel && (
+        <Formulario/>
+      )}
+
+      <button onClick={() => setFromularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button>
     </>
   )
 }
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Formulario = () => {
-    let [materiaA, setMateriaA] = useState(0);
-    let [materiaB, setMateriaB] = useState(0);
-    let [materiaC, setMateriaC] = useState(0);
-    let [nome, setNome] = useState(''); {/* Um estado diferente */}
+    const [materiaA, setMateriaA] = useState(0);
+    const [materiaB, setMateriaB] = useState(0);
+    const [materiaC, setMateriaC] = useState(0);
+    const [nome, setNome] = useState(''); // Um estado diferente
+
+    // useEffect para componentes iniciados utilizando o Array vasiu
+    useEffect(() => {
+        console.log("O ecomponente iniciou");
+
+        return () => {
+            console.log("O componente finalizou")
+        }
+
+    }, []);
+
+    // useEffect para nome
+    useEffect(() => {
+        console.log("O estado nome mudou");
+    }, [nome]);
+
+    // useEffect para os campos de notas
+    useEffect(() => {
+        console.log("Materia A mudou para: " + materiaA);
+    }, [materiaA, materiaB, materiaC]);
 
     const alteraNome = (evento) => {
         //console.log(evento.target.value)
         //setNome(evento.target.value);
         setNome(estadoAnterior => {
-            console.log(estadoAnterior);
+            //console.log(estadoAnterior);
 
             //estadoAnterior = valornovo
 
@@ -22,8 +42,8 @@ const Formulario = () => {
         const soma = materiaA + materiaB + materiaC;
         const media = soma / 3;
 
-        console.log(soma)
-        console.log(media)
+        //console.log(soma)
+        //console.log(media)
 
         if(media >= 7) {
             return (
