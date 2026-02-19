c926449a51c255b297f05fc22dd08c77b3d8dcf9
Aldo Santos <aldoacesso@gmail.com>
2026-02-18 22:30:20 -0300

    Código refatorado

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
 
 function App() {
-  const [formularioEstaVisivel, setFromularioEstaVisivel] = useState(true);
-  const [nomeUsuario, setNomeUsuario ] = useState('');
+  const [nomeUsuario, setNomeUsuario] = useState("");
 
   return (
-    <> 
+    <>
       <div>
-          <h1>Github Perfil</h1>
+        <h1>Github Perfil</h1>
       </div>
-      <input type="text" placeholder="Digite um usuário do Github" onBlur={(e) => setNomeUsuario(e.target.value) } />
+      <input
+        type="text"
+        placeholder="Digite um usuário do Github"
+        onBlur={(e) => setNomeUsuario(e.target.value)}
+      />
       <button className="btn-app">Clique aqui</button>
-      
+
       {nomeUsuario.length > 4 && (
         <>
-          <Perfil nomeUsuario={nomeUsuario}/>
-          <ReposList nomeUsuario={nomeUsuario}/>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
         </>
       )}
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
